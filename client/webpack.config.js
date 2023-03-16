f89fee95f04efcbd3466a5792f34c6ec2f240383
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-03-15 18:22:51 -0700

    added to webpack.config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,13 +17,36 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+      hot: 'only'
+    },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: 'Webpack Plugin'
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
